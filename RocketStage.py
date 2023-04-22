af63d2789e4d8c0908f68f4777377901eac59761
Gillermoo <williamecraver@gmail.com>
2023-04-22 11:55:48 -0400

    added fuels and fue tank files in progress

---
+++ RocketStage.py
@@ -0,0 +1,12 @@
+class RocketStage:
+    def __init__(self, mass, engine, numEngines, cost, type, dv, fuel, LF, OX, xenon):
+        self.mass = mass
+        self.engine = engine
+        self.numEngines = numEngines
+        self.cost = cost
+        self.type = type
+        self.dv = dv
+        self.fuel = fuel
+        self.LF = LF
+        self.OX = OX
+        self.xenon = xenon
