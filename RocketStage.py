92dbe3c48328a39b782ea5661fb11b4912eb31c6
Gillermoo <williamecraver@gmail.com>
2023-04-23 21:25:20 -0400

    added Rocket stage class with a linear stage child class

--- RocketStage.py
+++ RocketStage.py
@@ -34,8 +34,52 @@ class RocketStage:
                                                                        self.fuel)
         print(str2)
 
+    @staticmethod
+    def setup_dv_pl_arrays(dv, pl, num_engines):
+
+        return dv_array, pl_array
+
     @classmethod
-    def optimize_point(cls, pl, dv, max_eng_quant, asl_or_vac):
+    def setup_physics_arrays(cls, dv, pl, eng_type, max_eng_quant, asl_or_vac):
+        engines = [eng for eng in cls.engines if eng.fuel_type == eng_type]
+        num_engines = cls.count_rep(max_eng_quant, len(engines))
+
+        dv_array, pl_array = np.meshgrid(dv, pl, indexing='ij')
+        dv_array = np.expand_dims(dv_array, 2)
+        np.tile(dv_array, (1, 1, len(engines)))
+
+        pl_array = np.expand_dims(pl_array, 2)
+        np.tile(pl_array, (1, 1, len(engines)))
+
+        if asl_or_vac == 'asl':
+            isp = np.array([eng.isp_asl for eng in engines])
+            T = np.array([eng.thrust_asl for eng in engines])
+        elif asl_or_vac == 'vac':
+            isp = np.array([eng.isp_vac for eng in engines])
+            T = np.array([eng.thrust_vac for eng in engines])
+        else:
+            raise NotImplementedError('Other values for asl_or_vac have not been implemented')
+        isp = np.tile(isp, max_eng_quant)
+        isp = np.reshape(isp, [1, 1, len(num_engines)])
+
+        T = np.tile(T, max_eng_quant) * num_engines
+        T = np.reshape(T, [1, 1, len(num_engines)])
+
+        eng_mass = [eng.mass for eng in engines]
+        eng_mass = np.tile(eng_mass, max_eng_quant) * num_engines
+        eng_mass = np.reshape(eng_mass, [1, 1, len(num_engines)])
+
+        eng_cost = [eng.cost for eng in engines]
+        eng_cost = np.tile(eng_cost, max_eng_quant) * num_engines
+        eng_cost = np.reshape(eng_cost, [1, 1, len(num_engines)])
+
+        eng_name = [eng.name for eng in engines]
+        eng_name = np.tile(eng_name, max_eng_quant)
+
+        return dv_array, pl_array, isp, T, eng_mass, eng_cost, eng_name, num_engines
+
+    @classmethod
+    def optimize_point(cls, pl, dv, max_eng_quant, asl_or_vac, TWR_req):
         """
         Optimizes a rocket stage for a given point
 
@@ -45,7 +89,7 @@ class RocketStage:
         raise NotImplementedError(f"""optimize_point() function is not implemented for {cls.__class__}""")
 
     @classmethod
-    def optimize_plot(cls, pl_span, dv_span, span, max_eng_quant, asl_or_vac):
+    def optimize_plot(cls, pl_span, dv_span, span, max_eng_quant, asl_or_vac, TWR_req):
         """
         Optimizes a rocket stage for a given sweep of points
 
@@ -66,11 +110,11 @@ class PayloadStage(RocketStage):
         super().__init__(pl, [], 0, 0, 'PL', 0, 0, 0, 0, 0)
 
     @classmethod
-    def optimize_point(cls, pl, dv, max_eng_quant, asl_or_vac):
+    def optimize_point(cls, pl, dv, max_eng_quant, asl_or_vac, TWR_req):
         return cls(pl)
 
     @classmethod
-    def optimize_plot(cls, pl_span, dv_span, span, max_eng_quant, asl_or_vac):
+    def optimize_plot(cls, pl_span, dv_span, span, max_eng_quant, asl_or_vac, TWR_req):
         pass
 
 
@@ -79,40 +123,24 @@ class LinerStage(RocketStage):
         super().__init__(mass, engine, num_engines, cost, 'Linear', dv, fuel, lf, ox, xenon)
 
     @classmethod
-    def optimize_plot(cls, pl_span, dv_span, span, max_eng_quant, asl_or_vac):
+    def optimize_plot(cls, pl_span, dv_span, span, max_eng_quant, asl_or_vac, TWR_req):
         pl = np.logspace(np.log10(pl_span[0]), np.log10(pl_span[1]), span)
         dv = np.linspace(dv_span[0], dv_span[1], span)
 
         # Group engines by fuel type
         unique_eng_types = set([eng.fuel_type for eng in cls.engines])
 
+        all_engines = []
+        all_quant_engines = []
+
+        all_mtot = np.empty([span, span, 0])
+        all_costs = np.empty([span, span, 0])
+
         for eng_type in unique_eng_types:
-            engines = [eng for eng in cls.engines if eng.fuel_type == eng_type]
-            num_engines = cls.count_rep(max_eng_quant, len(engines))
-
-            dv_array, pl_array = np.meshgrid(dv, pl, indexing='ij')
-            dv_array = np.expand_dims(dv_array, 2)
-            np.tile(dv_array, (1, 1, len(num_engines)))
-
-            pl_array = np.expand_dims(pl_array, 2)
-            np.tile(pl_array, (1, 1, len(num_engines)))
-
-            if asl_or_vac == 'asl':
-                isp = np.array([eng.isp_asl for eng in engines])
-            elif asl_or_vac == 'vac':
-                isp = np.array([eng.isp_vac for eng in engines])
-            else:
-                raise NotImplementedError('Other values for asl_or_vac have not been implemented')
-            isp = np.tile(isp, max_eng_quant)
-            isp = np.reshape(isp, [1, 1, len(num_engines)])
-
-            eng_mass = [eng.mass for eng in engines]
-            eng_mass = np.tile(eng_mass, max_eng_quant) * num_engines
-            eng_mass = np.reshape(eng_mass, [1, 1, len(num_engines)])
-
-            eng_cost = [eng.cost for eng in engines]
-            eng_cost = np.tile(eng_cost, max_eng_quant) * num_engines
-            eng_cost = np.reshape(eng_cost, [1, 1, len(num_engines)])
+            dv_array, pl_array, isp, T, eng_mass, eng_cost, eng_name, num_engines = cls.setup_physics_arrays(dv, pl,
+                                                                                                             eng_type,
+                                                                                                             max_eng_quant,
+                                                                                                             asl_or_vac)
 
             best_empty_fraction = cls.tanks.__getattribute__(eng_type)['percent_structure']
 
@@ -129,5 +157,15 @@ class LinerStage(RocketStage):
                     mf * Fuels.get_fuel_data(eng_type, 'Cost')
             # Calculate m_tot which is the Structure + fuel + PL + engine masses
             m_tot = m100 + pl_array + eng_mass
+            TWR0 = T / m_tot
 
-            print(1)
+            all_engines.append(eng_name)
+            all_quant_engines.append(num_engines)
+
+            m_tot[TWR0 < TWR_req] = np.inf
+            costs[TWR0 < TWR_req] = np.inf
+
+            all_mtot = np.concatenate((all_mtot, m_tot), axis=2)
+            all_costs = np.concatenate((all_costs, costs), axis=2)
+
+        best_mass_idx =
